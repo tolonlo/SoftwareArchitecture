5bf4ce0a84c167d994eaad85b6edc8095ff15443
Tomás Londoño Lopera <144451050+tolonlo@users.noreply.github.com>
2025-07-29 19:21:12 -0500

    About Button

--- Tutoriales/tutorial1/helloworld_project/pages/views.py
+++ Tutoriales/tutorial1/helloworld_project/pages/views.py
@@ -8,3 +8,16 @@ def homePageView(request):
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html' 
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+        return context
